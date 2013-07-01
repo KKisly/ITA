248fc794e07709669fdfbb4f6267401f56466047
KKisly <kisly.k@gmail.com>
2013-07-01 01:46:11 -0700

    test

---
+++ HW_17/run_script_17_01.bat
@@ -0,0 +1,12 @@
+:: Commands to execute script script_00_01.rb
+
+:: ruby ./lib/script_00_01.rb apple banana
+
+ruby ./lib/script_17_01.rb --first apple --second banana
+ruby ./lib/script_17_02.rb --first apples --second bananas
+ruby ./lib/script_17_03.rb --first 20 --second 2
+ruby ./lib/script_17_04.rb -a 66 -b 166 -c 202 -d 14
+ruby ./lib/script_17_05.rb -a 35 -b 45 -c 61 -d 59 -e 73
+ruby ./lib/script_17_06.rb --first_number 50 --second_number 20 --third_number 30
+ruby ./lib/script_17_07.rb --sentence Wednesday morning, John Smith was walking on the street.
+ruby ./lib/script_17_08.rb -f Spring -s Summer -t Fall -f Winter
