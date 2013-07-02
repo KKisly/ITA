7fb1222c333dc2f51e1d4a9debac960e393fe41f
KKisly <kisly.k@gmail.com>
2013-07-02 03:35:44 -0700

    hw21

--- hw_21/run_script_21_01.bat
+++ hw_21/run_script_21_01.bat
@@ -9,6 +9,6 @@ ruby ./lib/script_21_04.rb --input ./etc/input_21_04.txt
 ruby ./lib/script_21_05.rb --input ./etc/input_21_05.txt
 ruby ./lib/script_21_06.rb --input ./etc/input_21_06.txt
 ruby ./lib/script_21_06.rb --input ./etc/input_21_06a.txt
-ruby ./lib/script_21_02.rb --input ./etc/input_21_02.txt
-ruby ./lib/script_21_02.rb --input ./etc/input_21_02.txt
-ruby ./lib/script_21_02.rb --input ./etc/input_21_02.txt
+ruby ./lib/script_21_07.rb --input ./etc/input_21_07.txt
+ruby ./lib/script_21_07.rb --input ./etc/input_21_07a.txt
+ruby ./lib/script_21_08.rb --input ./etc/input_21_08.txt
