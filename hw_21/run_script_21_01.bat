721d89815f7a448558e916f9ee9078ae6d8ac296
KKisly <kisly.k@gmail.com>
2013-07-02 03:26:22 -0700

    hw21

--- hw_21/run_script_21_01.bat
+++ hw_21/run_script_21_01.bat
@@ -4,9 +4,11 @@
 
 ruby ./lib/script_21_01.rb --input ./etc/input_21_01.txt
 ruby ./lib/script_21_02.rb --input ./etc/input_21_02.txt
-::ruby ./lib/script_17_03.rb --first 20 --second 2
-::ruby ./lib/script_17_04.rb -a 66 -b 166 -c 202 -d 14
-::ruby ./lib/script_17_05.rb -a 35 -b 45 -c 61 -d 59 -e 73
-::ruby ./lib/script_17_06.rb --first_number 50 --second_number 20 --third_number 30
-::ruby ./lib/script_17_07.rb --sentence Wednesday morning, John Smith was walking on the street.
-::ruby ./lib/script_17_08.rb -f Spring -s Summer -t Fall -f Winter
+ruby ./lib/script_21_03.rb --input ./etc/input_21_03.txt
+ruby ./lib/script_21_04.rb --input ./etc/input_21_04.txt
+ruby ./lib/script_21_05.rb --input ./etc/input_21_05.txt
+ruby ./lib/script_21_06.rb --input ./etc/input_21_06.txt
+ruby ./lib/script_21_06.rb --input ./etc/input_21_06a.txt
+ruby ./lib/script_21_02.rb --input ./etc/input_21_02.txt
+ruby ./lib/script_21_02.rb --input ./etc/input_21_02.txt
+ruby ./lib/script_21_02.rb --input ./etc/input_21_02.txt
