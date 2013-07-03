64cd5083a3c0d8f3a60753fb2f03c56273d12255
KKisly <kisly.k@gmail.com>
2013-07-02 21:16:32 -0700

    hw21

--- hw_21/run_script_21_01.bat
+++ hw_21/run_script_21_01.bat
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-:: Commands to execute script script_00_01.rb
-
-:: ruby ./lib/script_00_01.rb apple banana
-
-ruby ./lib/script_21_01.rb --input ./etc/input_21_01.txt
-ruby ./lib/script_21_02.rb --input ./etc/input_21_02.txt
-::ruby ./lib/script_17_03.rb --first 20 --second 2
-::ruby ./lib/script_17_04.rb -a 66 -b 166 -c 202 -d 14
-::ruby ./lib/script_17_05.rb -a 35 -b 45 -c 61 -d 59 -e 73
-::ruby ./lib/script_17_06.rb --first_number 50 --second_number 20 --third_number 30
-::ruby ./lib/script_17_07.rb --sentence Wednesday morning, John Smith was walking on the street.
-::ruby ./lib/script_17_08.rb -f Spring -s Summer -t Fall -f Winter
-=======
 :: Commands to execute script script_00_01.rb
 
 :: ruby ./lib/script_00_01.rb apple banana
@@ -26,4 +12,3 @@ ruby ./lib/script_21_06.rb --input ./etc/input_21_06a.txt
 ruby ./lib/script_21_07.rb --input ./etc/input_21_07.txt
 ruby ./lib/script_21_07.rb --input ./etc/input_21_07a.txt
 ruby ./lib/script_21_08.rb --input ./etc/input_21_08.txt
->>>>>>> 4edb9d0c3610af5b5a2a3bca734f8da607f3821c
